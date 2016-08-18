be343b34784540da56382f3cfff62053d91e364e
Pranjal Kumar <pranjal.kr94@gmail.com>
2016-08-18 16:18:38 +0530

    react with webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var config = {
+   entry: './static/js/main.js',
+    
+   output: {
+      path:'./static/js/',
+      filename: 'index.js',
+   },
+    
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+    
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+                
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
