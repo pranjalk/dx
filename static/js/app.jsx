6ea1d4aa095e4a194dc35272de2ce4da131479f6
Pranjal Kumar <pranjal.kr94@gmail.com>
2016-08-19 11:26:13 +0530

    patched codeclimate config

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!!!
-         </div>
-      );
-   }
+  render() {
+    return (
+      <div>Hello World!!!</div>
+    );
+  }
 }
-
 export default App;
