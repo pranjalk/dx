a2a0d46b0cf28d408393678738d469b3d1b27cf1
Pranjal Kumar <pranjal.kr94@gmail.com>
2016-08-19 15:09:42 +0530

    linted server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-
-const path = require('path');
 
 app.use(express.static('static'));
 app.set('views', path.join(__dirname, '/view'));
 
 app.engine('html', require('ejs').renderFile);
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.render('index.html');
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('dx server listening on port 3000!');
 });
