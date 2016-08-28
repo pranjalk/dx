565e623d258c07b2475e5b391354889f19047164
Pranjal Kumar <pranjal.kr94@gmail.com>
2016-08-28 20:02:49 +0530

    gulp file fix

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,23 @@ const rename = require('gulp-rename');
 
 gulp.task('default', ['styles', 'scripts']);
 
-gulp.task('styles', () => 
+gulp.task('styles', () => {
   return gulp.src('./dist/css/styles.css')
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false,
-    ))
+    }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cssnano())
     .pipe(gulp.dest('./dist/css'))
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
-gulp.task('scripts', () =>
+gulp.task('scripts', () =>{
   return gulp.src('./dist/js/index.js')
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js'))
     .pipe(notify({ message: 'Scripts task complete' }));
-);
+});
 
