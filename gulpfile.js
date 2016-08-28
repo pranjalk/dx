39d5da2c19e634396a685b56a04edc5b310dc73a
Pranjal Kumar <pranjal.kr94@gmail.com>
2016-08-28 19:39:03 +0530

    fixed linting for gulp file in code climate

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cssnano = require('gulp-cssnano'),
-    uglify = require('gulp-uglify'),
-    notify = require('gulp-notify'),
-    rename = require('gulp-rename');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
+const notify = require('gulp-notify');
+const rename = require('gulp-rename');
 
-gulp.task('default', ['styles','scripts']);
+gulp.task('default', ['styles', 'scripts']);
 
 gulp.task('styles', () => {
   return gulp.src('./dist/css/styles.css')
@@ -13,17 +13,17 @@ gulp.task('styles', () => {
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(cssnano())
     .pipe(gulp.dest('./dist/css'))
-    .pipe(notify({ message: 'Styles task complete' }))
+    .pipe(notify({ message: 'Styles task complete' }));
 });
 
 gulp.task('scripts', () =>{
   return gulp.src('./dist/js/index.js')
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js'))
-    .pipe(notify({ message: 'Scripts task complete' }))
+    .pipe(notify({ message: 'Scripts task complete' }));
 });
 
